97041cc5b2022020714fea4205c17a92c84784b5
Grahahax <grahahax@uk.ibm.com>
2016-11-27 21:39:48 +0000

    Add mem and disk usage

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -9,11 +9,20 @@ install_node_modules() {
     else
       echo "Installing node modules (package.json)"
     fi
+    echo "mem and disk before install"
+    free -mh
+    df -h
     echo "Going to install"
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    echo "mem and disk post install"
+    free -mh
+    df -h
     echo "Installed, going to dedupe"
     npm dedupe 2>&1
     echo "Dedupe done"
+    echo "mem and disk after dedupe"
+    free -mh
+    df -h
   else
     echo "Skipping (no package.json)"
   fi
